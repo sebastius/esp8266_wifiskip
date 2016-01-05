32b084b19b233460ab96fa19ee6d3cf7baf05ef4
Sebastius <bas@knutselaar.net>
2016-01-05 22:09:42 +0100

    Troll is back!

--- esp8266_wifiskip.ino
+++ esp8266_wifiskip.ino
@@ -9,8 +9,6 @@
  * On the push of a button constructs an HTTP-GET request and sends it to our
  * squeezer-server. Based on a generic HTTP Get example, generic WiFi connecting code etc.
  *
- *
- * test
  */
 
 #include <ESP8266WiFi.h>
@@ -23,7 +21,7 @@ const char pass[] = "";
 // Remote site information
 const char http_site[] = "jukebox.space.revspace.nl";
 const int http_port = 80;
-// const char player[] = "00%3A00%3A00%3A00%3A00%3A00"; // bar sparkshack
+// const char player[] = "d8%3Ad3%3A85%3A17%3A30%3A9c"; // bar sparkshack
 const char player[] = "be%3Ae0%3Ae6%3A04%3A46%3A38"; // klusbunker
 
 
@@ -36,14 +34,17 @@ const char emptypar[] = "%2B1";
 const char volume[] = "volume";
 const char highvolume[] = "%2B5";
 const char lowvolume[] = "-5";
+const char insertcommand[] = "insert";
+const char troll[] = "file%3A%2F%2F%2Fmusic%2FLosse%20Tracks%2Ftroll.mp3";
 
-// Pin definitions
+                     // Pin definitions
 
 const int skipbutton = 0;
 const int volhighbutton = 12;
 const int vollowbutton = 13;
 const int stopbutton = 5;
-const int randombutton = 4;
+const int trollbutton = 4;
+
 
 // Global variables
 WiFiClient client;
@@ -55,7 +56,7 @@ void setup() {
   pinMode(volhighbutton, INPUT);
   pinMode(vollowbutton, INPUT);
   pinMode(stopbutton, INPUT);
-  pinMode(randombutton, INPUT);
+  pinMode(trollbutton, INPUT);
 
   // Lets get the Serial started
   Serial.begin(115200);
@@ -74,58 +75,58 @@ void setup() {
 
 void loop() {
 
-
-  // If there are incoming bytes, print them
-//    if ( client.available() ) {
-//    char c = client.read();
-//    Serial.print(c);
+//  if (digitalRead(skipbutton) == LOW) { // Normally Open button, so when tied to ground (LOW) button is pressed)
+//    Serial.println("Skip");
+//    getPage(playlist, jumpcommand , emptypar);
+//    delay(500);
+//    // An ugly check to see if the skipbutton is still pressed after 3 seconds.
+//    // Then load a new randomized playlist to the player.
+//    // Did the staircase "if" to be more responsive after a button press, otherwise the skip button would be disabled for 3 seconds!
+//    if (digitalRead(skipbutton) == LOW) {
+//      delay(1000);
+//      if (digitalRead(skipbutton) == LOW) {
+//        delay(1000);
+//        Serial.println("Randomize");
+//        newrandomplaylist();
+//        delay(2000);
+//      }
 //    }
+//  }
+//
+//  if (digitalRead(volhighbutton) == LOW) { // HIGH because i use Normaly Close instead of NOpen buttons)
+//    Serial.println("Volume HIGH");
+//    getPage(mixer, volume , highvolume);
+//    delay(10);
+//  }
+//
+//  if (digitalRead(vollowbutton) == LOW) { // HIGH because i use Normaly Close instead of NOpen buttons)
+//    Serial.println("Volume Low");
+//    getPage(mixer, volume , lowvolume);
+//    delay(10);
+//  }
+//
+//  if (digitalRead(stopbutton) == LOW) { // HIGH because i use Normaly Close instead of NOpen buttons)
+//    //stop
+//
+//    Serial.println("Stop");
+//    getPage(stopcommand, emptypar , emptypar);
+//    delay(100);
+//  }
 
-
-  if (digitalRead(skipbutton) == LOW) { // Normally Open button, so when tied to ground (LOW) button is pressed)
-    Serial.println("Skip");
+  if (digitalRead(skipbutton) == LOW) { // HIGH because i use Normaly Close instead of NOpen buttons)
+    Serial.println("Trollolol");
+    Serial.println(playlist);
+    Serial.println(insertcommand);
+    Serial.println(troll);
+    getPage(playlist, insertcommand, troll);
+    delay(50);
     getPage(playlist, jumpcommand , emptypar);
     delay(500);
-    // An ugly check to see if the skipbutton is still pressed after 3 seconds.
-    // Then load a new randomized playlist to the player.
-    // Did the staircase "if" to be more responsive after a button press, otherwise the skip button would be disabled for 3 seconds!
-    if (digitalRead(skipbutton) == LOW) {
-      delay(1000);
-      if (digitalRead(skipbutton) == LOW) {
-        delay(1000);
-        Serial.println("Randomize");
-        newrandomplaylist();
-        delay(2000);
-      }
-    }
   }
 
-  if (digitalRead(volhighbutton) == HIGH) { // HIGH because i use Normaly Close instead of NOpen buttons)
-    Serial.println("Volume HIGH");
-    getPage(mixer, volume , highvolume);
-    //delay(100);
-  }
 
-  if (digitalRead(vollowbutton) == HIGH) { // HIGH because i use Normaly Close instead of NOpen buttons)
-    Serial.println("Volume Low");
-    getPage(mixer, volume , lowvolume);
-    //delay(100);
-  }
 
-  if (digitalRead(stopbutton) == HIGH) { // HIGH because i use Normaly Close instead of NOpen buttons)
-    //stop
 
-    Serial.println("Stop");
-    getPage(stopcommand, emptypar , emptypar);
-    //delay(1000);
-  }
-
-  if (digitalRead(randombutton) == HIGH) { // HIGH because i use Normaly Close instead of NOpen buttons)
-    // http://jukebox/Classic/plugins/RandomPlay/mix.html?type=track&player=00%3A00%3A00%3A00%3A00%3A00&addOnly=0
-    Serial.println("Randomize");
-    newrandomplaylist();
-    //delay(1000);
-  }
 
 }
 
